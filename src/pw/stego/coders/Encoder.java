738940e6792d4f58a0579a2244582c395ecce89e
LabunskyA <log.me.here.now.yeah@gmail.com>
2016-07-10 00:04:06 +0300

    Coders update

--- src/pw/stego/coders/Encoder.java
+++ src/pw/stego/coders/Encoder.java
@@ -1,6 +1,7 @@
 package pw.stego.coders;
 
 import pw.stego.Block;
+import pw.stego.Task;
 
 import java.awt.image.BufferedImage;
 
@@ -8,18 +9,15 @@ import java.awt.image.BufferedImage;
  * Class for encoding message to container
  */
 public class Encoder extends Coder{
-    public int hideData(BufferedImage to, Block[] data, int from) {
+    private int hideData(BufferedImage to, Block[] data, int from) {
         System.out.println("Encoding from x = " + from % to.getWidth() + " y = " + from / to.getWidth());
-        return encode(to, data, from);
-    }
 
-    private int encode(BufferedImage to, Block[] data, int from) {
         int length = to.getWidth();
 
         int i, j;
         for (i = 0, j = from; i < data.length; i++) {
             to.setRGB(j % length, j / length, toEncoded(to.getRGB(j % length, j / length), data[i].value));
-            System.out.print(toDecoded(to.getRGB(j % length, j / length)));
+//            System.out.print(toDecoded(to.getRGB(j % length, j / length)));
 
             switch (data[i].type) {
                 case TRANS:
@@ -40,8 +38,38 @@ public class Encoder extends Coder{
             }
         }
 
-        System.out.println();
+//        System.out.println();
 
         return j;
+    }
+
+    public boolean encode(Task task) {
+        BufferedImage image = task.getImage();
+
+        int length = image.getWidth();
+        int i = 0;
+
+        try {
+            while (task.nextDataPart()) {
+                i = hideData(image, task.getData(), task.getFrom().y * length + task.getFrom().x);
+
+                if (task.getMeta() != null) {
+                    image.setRGB(i % length, i / length, (image.getRGB(i % length, i / length) & 0xfffffefe) |
+                            0x10101);
+
+                    hideData(image, task.getMeta(), i + delta);
+                }
+            }
+
+//            System.out.println("Encoding from x = " + i % length + " y = " + i / length);
+            image.setRGB(i % length, i / length, (image.getRGB(i % length, i / length) & 0xfffffefe) |
+                    0x10000);
+//            System.out.println(4);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 }
