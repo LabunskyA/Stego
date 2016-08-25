b425743aaf9fd0be61d1bb3304428f111b246a25
LabunskyA <log.me.here.now.yeah@gmail.com>
2016-08-25 17:38:54 +0300

    Merge?

--- src/pw/userspace/StandAlone.java
+++ src/pw/userspace/StandAlone.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * StandAlone class with main method and arguments processing
@@ -22,27 +23,32 @@ class StandAlone {
     private StandAlone(String[] args) throws IOException, IllegalArgumentException {
         if (args.length < 3) {
             System.out.println("Arguments for decoding: \"path_to_container\" --decode \"path_to_key\"");
-            System.out.println("Arguments for encoding: \"path_to_container\" --encode \"path_to_pattern\" " +
-                    "\"path_to_message\"");
+            System.out.println("Arguments for encoding: \"path_to_container\" --encode \"path_to_key\"" +
+                    " \"path_to_pattern\" \"path_to_message\"");
         }
 
+        byte[] key = Files.readAllBytes(Paths.get(args[0]));
+
         File imageFile;
-        if (!(imageFile = new File(args[0])).exists())
+        if (!(imageFile = new File(args[1])).exists())
             throw new FileNotFoundException();
 
-        String pattern = new String(Files.readAllBytes(new File(args[2]).toPath()));
+        String pattern = new String(Files.readAllBytes(new File(args[3]).toPath()));
         byte[] data;
         try {
-            data = Files.readAllBytes(new File(args[3]).toPath());
+            data = Files.readAllBytes(new File(args[4]).toPath());
         } catch (ArrayIndexOutOfBoundsException e) {
-            data = Files.readAllBytes(new File(args[2]).toPath());
+            data = Files.readAllBytes(new File(args[3]).toPath());
         }
 
         if (!args[1].equals("--encode") && !args[1].equals("--decode"))
             throw new IllegalArgumentException();
 
         Task.Type type = args[1].equals("--encode") ? Task.Type.ENCODE : Task.Type.DECODE;
-        handler = new Handler(new Task(type, imageFile, pattern, data));
+        if (type == Task.Type.DECODE)
+            handler = new Handler(new Task(imageFile, data));
+        else handler = new Handler(new Task(imageFile, data, key, pattern));
+
         System.out.println(new String(data));
     }
 
